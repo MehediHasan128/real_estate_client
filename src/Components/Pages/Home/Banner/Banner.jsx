8255945267f83adb946f160c1a02521cc1f1859d
MehediHasan128 <mehedihasan12926@gmail.com>
2024-01-04 16:37:56 +0600

    add swiper js in banner section

--- src/Components/Pages/Home/Banner/Banner.jsx
+++ src/Components/Pages/Home/Banner/Banner.jsx
@@ -1,22 +1,35 @@
-import { Box, Typography } from "@mui/material";
 import "./Banner.css";
+import { Box, Typography } from "@mui/material";
+import { motion } from "framer-motion";
 
-// Import social media Icon
-import FacebookIcon from '@mui/icons-material/Facebook';
-import YouTubeIcon from '@mui/icons-material/YouTube';
-import InstagramIcon from '@mui/icons-material/Instagram';
+// Import material icons
+import FacebookIcon from "@mui/icons-material/Facebook";
+import InstagramIcon from "@mui/icons-material/Instagram";
+import YouTubeIcon from "@mui/icons-material/YouTube";
+import BookmarkIcon from "@mui/icons-material/Bookmark";
+
+// Import slider image
+import img1 from "../../../../assets/bannerSlider/slider1.jpg";
+import img2 from "../../../../assets/bannerSlider/slider2.jpg";
+import img3 from "../../../../assets/bannerSlider/slider3.jpg";
+import img4 from "../../../../assets/bannerSlider/slider4.jpg";
+import img5 from "../../../../assets/bannerSlider/slider5.jpg";
+
+// Swiper js
+import { Swiper, SwiperSlide } from "swiper/react";
+import { EffectFade, Autoplay, Pagination, Navigation } from "swiper/modules";
 
 const Banner = () => {
   return (
-    <div className="background min-h-screen flex items-center bg-fixed px-5 lg:px-64">
-      <Box className="flex flex-col lg:flex-row">
+    <div className="background min-h-screen flex bg-fixed px-5 lg:px-64 pb-24 lg:pb-0">
+      <Box className="flex flex-col-reverse lg:flex-row items-center gap-10 lg:gap-20 mt-36">
         {/* Banner Content */}
-        <Box className="text-white space-y-5 lg:space-y-10">
+        <Box className="text-white space-y-8 lg:space-y-10">
           {/* Text Content */}
           <Box>
             <Typography variant="h6">Welcome To,</Typography>
             <Typography variant="h1">
-              <p className="tracking-widest font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#7b4397] to-[#dc2430]">
+              <p className="tracking-widest font-bold text-transparent bg-clip-text bg-gradient-to-r from-[#70318f] to-[#f01f2d]">
                 LuxeLoom
               </p>
             </Typography>
@@ -24,7 +37,7 @@ const Banner = () => {
               <p className="tracking-[0.5em] md:tracking-[0.6em]">Properties</p>
             </Typography>
             <Typography variant="h6">
-              <p className="mt-5 text-justify lg:w-[60%]">
+              <p className="mt-5 text-justify">
                 Where Dreams Find a Home. Discover the pinnacle of luxury living
                 with our exquisite properties. Your journey to a refined
                 lifestyle starts here.
@@ -35,21 +48,73 @@ const Banner = () => {
           {/* Social Button */}
           <Box className="flex gap-5 lg:gap-8">
             <Box className="bg-slate-400 w-fit p-[3px] rounded-full bg-gradient-to-r from-[#7b4397] to-[#dc2430] hover:-translate-y-2 duration-500 cursor-pointer">
-                <div className="p-1 lg:p-2 bg-black rounded-full">
-                    <FacebookIcon sx={{width: '30px', height: '30px'}} />
-                </div>
+              <div className="p-1 lg:p-2 bg-black rounded-full">
+                <FacebookIcon sx={{ width: "30px", height: "30px" }} />
+              </div>
             </Box>
             <Box className="bg-slate-400 w-fit p-[3px] rounded-full bg-gradient-to-r from-[#7b4397] to-[#dc2430] hover:-translate-y-2 duration-500 cursor-pointer">
-                <div className="p-1 lg:p-2 bg-black rounded-full">
-                    <YouTubeIcon sx={{width: '30px', height: '30px'}} />
-                </div>
+              <div className="p-1 lg:p-2 bg-black rounded-full">
+                <YouTubeIcon sx={{ width: "30px", height: "30px" }} />
+              </div>
             </Box>
             <Box className="bg-slate-400 w-fit p-[3px] rounded-full bg-gradient-to-r from-[#7b4397] to-[#dc2430] hover:-translate-y-2 duration-500 cursor-pointer">
-                <div className="p-1 lg:p-2 bg-black rounded-full">
-                    <InstagramIcon sx={{width: '30px', height: '30px'}} />
-                </div>
+              <div className="p-1 lg:p-2 bg-black rounded-full">
+                <InstagramIcon sx={{ width: "30px", height: "30px" }} />
+              </div>
             </Box>
           </Box>
+
+          {/* Button section */}
+          <Box className="flex gap-8">
+            <motion.button
+              className="bg-gradient-to-r from-[#7b4397] to-[#dc2430] px-3 md:px-5 md:py-3 flex items-center gap-2 rounded-xl"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            >
+              <Typography variant="body1">
+                Book Now
+                <BookmarkIcon />
+              </Typography>
+            </motion.button>
+            <motion.button
+              className="bg-gradient-to-r from-[#7b4397] to-[#dc2430] p-[2px] flex items-center gap-2 rounded-xl"
+              whileHover={{ scale: 1.2 }}
+              whileTap={{ scale: 0.9 }}
+              transition={{ type: "spring", stiffness: 400, damping: 17 }}
+            >
+              <div className="bg-black px-6 py-3 rounded-xl">Join Us</div>
+            </motion.button>
+          </Box>
+        </Box>
+
+        {/* Banner slider */}
+        <Box className="lg:w-[50%]">
+          <Swiper
+            className="w-[300px] lg:w-[400px] mySwiper"
+            effect={"fade"}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: true,
+            }}
+            modules={[EffectFade, Autoplay, Pagination, Navigation]}
+          >
+            <SwiperSlide>
+              <img className="object-cover rounded-xl h-[450px] lg:h-[600px]" src={img1} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="object-cover rounded-xl h-[450px] lg:h-[600px]" src={img2} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="object-cover rounded-xl h-[450px] lg:h-[600px]" src={img3} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="object-cover rounded-xl h-[450px] lg:h-[600px]" src={img4} alt="" />
+            </SwiperSlide>
+            <SwiperSlide>
+              <img className="object-cover rounded-xl h-[450px] lg:h-[600px]" src={img5} alt="" />
+            </SwiperSlide>
+          </Swiper>
         </Box>
       </Box>
     </div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,11 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
+import 'swiper/css';
+import 'swiper/css/pagination';
+import 'swiper/css/navigation';
+import 'swiper/css/effect-fade';
+
 // Roboto Font
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
@@ -11,7 +16,8 @@ import { RouterProvider } from 'react-router-dom';
 import router from './Components/Router/Router';
 
 // Responsive font
-import { ThemeProvider, createTheme, responsiveFontSizes } from '@mui/material/styles';
+import { ThemeProvider } from '@emotion/react'
+import { createTheme, responsiveFontSizes } from '@mui/material/styles';
 let theme = createTheme();
 theme = responsiveFontSizes(theme);
 
