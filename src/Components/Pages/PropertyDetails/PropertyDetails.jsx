c835b79db40afb574168a4e5a7053a6c80413dc4
MehediHasan128 <mehedihasan12926@gmail.com>
2024-01-25 01:03:41 +0600

    add property image on details page and add some information

--- src/Components/Pages/PropertyDetails/PropertyDetails.jsx
+++ src/Components/Pages/PropertyDetails/PropertyDetails.jsx
@@ -1,4 +1,4 @@
-import { Box, Container, Typography } from "@mui/material";
+import { Box, Container, Divider, Typography } from "@mui/material";
 import property from "../../../assets/Properties/property1.jpg";
 
 import { GrLocation } from "react-icons/gr";
@@ -17,25 +17,45 @@ const PropertyDetails = () => {
           </Typography>
 
           <Box className="bg-white rounded-xl p-5 shadow-lg mt-5">
-            <div className="mb-5">
-              <img
-                className="w-[60%] h-96 object-cover rounded-lg"
-                src={property}
-                alt=""
-              />
+            <div className="mb-5 flex gap-5">
+              <div className="w-[80%]">
+                <img
+                  className="w-[100%] h-[450px] object-cover rounded-lg"
+                  src={property}
+                  alt=""
+                />
+              </div>
+              <div className="h-[450px] w-[24%] flex flex-col gap-5">
+                <img
+                  className="w-[100%] h-full object-cover rounded-lg"
+                  src={property}
+                  alt=""
+                />
+                <div className="relative">
+                  <img
+                    className="w-[100%] object-cover rounded-lg brightness-75"
+                    src={property}
+                    alt=""
+                  />
+                  <div className="absolute top-0 w-full h-full rounded-lg flex justify-center items-center">
+                    <button className="px-10 py-3 bg-opacity-20 backdrop-blur-lg bg-white text-xl font-semibold rounded-xl text-white">
+                      +20
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
 
-            <Box>
-              <Typography variant="h5">
-                <p className="font-semibold">USD $ 68,000</p>
-              </Typography>
-              <Typography variant="body1">
-                <p className="flex items-center gap-1 my-2">
-                  <GrLocation className="text-xl" /> Dhaka, Bangladesh
-                </p>
-              </Typography>
-
-              <div className="flex items-center justify-between w-[60%]">
+            <Box className="flex gap-10 items-center">
+              <div>
+                <Typography variant="h5">
+                  <p className="font-semibold">USD $ 68,000</p>
+                </Typography>
+                <Typography variant="body1">
+                  <p className="flex items-center gap-1 my-2">
+                    <GrLocation className="text-xl" /> Dhaka, Bangladesh
+                  </p>
+                </Typography>
                 <div className="flex gap-5">
                   <Typography variant="body1">
                     <p className="flex items-center gap-2 font-medium">
@@ -54,9 +74,32 @@ const PropertyDetails = () => {
                     </p>
                   </Typography>
                 </div>
-                <button className="px-5 py-3 bg-blue-500 font-semibold rounded-xl text-white">Booking Now</button>
+              </div>
+
+              <Divider orientation="vertical" variant="middle" flexItem />
+
+              <div>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="w-12 rounded-full">
+                      <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                    </div>
+                  </div>
+                  <div>
+                    <Typography variant="h6">
+                      <p>Mehedi Hasan</p>
+                    </Typography>
+                  </div>
+                </div>
+                <Typography variant="body1">
+                  <p className="font-medium mt-2">mehediHasan12926@gmail.com</p>
+                </Typography>
               </div>
             </Box>
+
+            <div className="my-5">
+              <Divider />
+            </div>
           </Box>
         </Box>
       </Container>
