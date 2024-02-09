ed72afcc3f663a4d16461953d41e50e1558bddbc
MehediHasan128 <mehedihasan12926@gmail.com>
2024-02-10 01:58:53 +0600

    add pie chart from material Ui

--- src/Components/Pages/AgentDashBoard/AgentProfile/AgentProfile.jsx
+++ src/Components/Pages/AgentDashBoard/AgentProfile/AgentProfile.jsx
@@ -1,88 +1,196 @@
 import { Box, Divider, Typography } from "@mui/material";
 import { MdOutlineVerified } from "react-icons/md";
-import { FaTwitter, FaFacebookF, FaLinkedinIn } from "react-icons/fa";
+import {
+  FaTwitter,
+  FaFacebookF,
+  FaLinkedinIn,
+  FaHandPointRight,
+} from "react-icons/fa";
+import { PieChart } from "@mui/x-charts/PieChart";
+
+const palette = ['#566573 ', '#7710BF', '#BF106A'];
 
 const AgentProfile = () => {
   return (
     <>
       <Box>
-        <Box className="flex gap-10">
-        <Box className="bg-white p-5 md:w-[60%] lg:w-[25%] rounded-lg">
-          <div>
-            <div className="avatar">
-              <div className="w-24 rounded-full">
-                <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+        <Box className="flex flex-col lg:flex-row gap-10">
+          <Box className="bg-white p-5 md:w-[60%] lg:w-[25%] rounded-lg">
+            <div>
+              <div className="avatar">
+                <div className="w-24 rounded-full">
+                  <img src="https://daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                </div>
+              </div>
+              <div className="flex items-center gap-2">
+                <Typography variant="h6">
+                  <p>Mehedi Hasan Bayzid</p>
+                </Typography>
+                <MdOutlineVerified className="text-xl text-blue-500" />
               </div>
             </div>
-            <div className="flex items-center gap-2">
-              <Typography variant="h6">
-                <p>Mehedi Hasan Bayzid</p>
-              </Typography>
-              <MdOutlineVerified className="text-xl text-blue-500" />
-            </div>
-          </div>
 
-          <div className="my-5">
-            <Divider />
-          </div>
+            <div className="my-5">
+              <Divider />
+            </div>
 
-          <Typography variant="caption text">
-           <div className="text-gray-600 space-y-1">
-           <div className="flex">
-                <p className="w-[25%]">Age:</p>
-                <p>26</p>
-            </div>
-            <div className="flex">
-                <p className="w-[25%]">City:</p>
-                <p>Uttara</p>
-            </div>
-            <div className="flex">
-                <p className="w-[25%]">State:</p>
-                <p>Dhaka</p>
-            </div>
-            <div className="flex">
-                <p className="w-[25%]">Country:</p>
-                <p>Bangladesh</p>
-            </div>
-            <div className="flex">
-                <p className="w-[25%]">Phone:</p>
-                <p>+8801302557956</p>
-            </div>
-            <div className="flex">
-                <p className="w-[25%]">Email:</p>
-                <p>mehedihasan12926@gmail.com</p>
-            </div>
-           </div>
-          </Typography>
+            <Typography variant="caption text">
+              <div className="text-gray-600 space-y-1">
+                <div className="flex">
+                  <p className="w-[25%]">Age:</p>
+                  <p>26</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[25%]">City:</p>
+                  <p>Uttara</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[25%]">State:</p>
+                  <p>Dhaka</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[25%]">Country:</p>
+                  <p>Bangladesh</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[25%]">Phone:</p>
+                  <p>+8801302557956</p>
+                </div>
+                <div className="flex">
+                  <p className="w-[25%]">Email:</p>
+                  <p>mehedihasan12926@gmail.com</p>
+                </div>
+              </div>
+            </Typography>
 
-          <div className="my-5">
-            <Divider />
-          </div>
+            <div className="my-10">
+              <Divider />
+            </div>
 
-          <div className="flex justify-center gap-5">
-            <div className="bg-blue-500 w-fit p-2 rounded-full shadow-xl text-white hover:bg-blue-600 hover:-translate-y-1 cursor-pointer duration-500">
+            <div className="flex justify-center gap-5">
+              <div className="bg-blue-500 w-fit p-2 rounded-full shadow-xl text-white hover:bg-blue-600 hover:-translate-y-1 cursor-pointer duration-500">
                 <FaFacebookF className="text-xl" />
-            </div>
-            <div className="bg-blue-500 w-fit p-2 rounded-full shadow-xl text-white hover:bg-blue-600 hover:-translate-y-1 cursor-pointer duration-500">
+              </div>
+              <div className="bg-blue-500 w-fit p-2 rounded-full shadow-xl text-white hover:bg-blue-600 hover:-translate-y-1 cursor-pointer duration-500">
                 <FaTwitter className="text-xl" />
-            </div>
-            <div className="bg-blue-500 w-fit p-2 rounded-full shadow-xl text-white hover:bg-blue-600 hover:-translate-y-1 cursor-pointer duration-500">
+              </div>
+              <div className="bg-blue-500 w-fit p-2 rounded-full shadow-xl text-white hover:bg-blue-600 hover:-translate-y-1 cursor-pointer duration-500">
                 <FaLinkedinIn className="text-xl" />
+              </div>
             </div>
-          </div>
-        </Box>
+          </Box>
 
-        <Box className="w-[50%] bg-white p-5 rounded-lg">
+          <Box className="lg:w-[50%] bg-white p-5 rounded-lg">
             <Typography variant="h5">
-                <p className="font-semibold">Agent Details</p>
+              <p className="font-semibold">Agent Details</p>
             </Typography>
+
             <div className="my-5 pr-10">
-                <Divider />
+              <Divider />
             </div>
+
             <Typography variant="body2">
-                <p className="text-justify text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo sint officia qui cum dicta iste nam velit quasi suscipit. Perferendis expedita, omnis molestias magnam nisi possimus, autem ad ut maxime adipisci reprehenderit quo, ipsam nam unde cumque ipsum deleniti porro ab impedit consequuntur dolores! Amet quidem vel debitis a nobis?</p>
+              <p className="text-justify text-gray-500">
+                Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo
+                sint officia qui cum dicta iste nam velit quasi suscipit.
+                Perferendis expedita, omnis molestias magnam nisi possimus,
+                autem ad ut maxime adipisci reprehenderit quo, ipsam nam unde
+                cumque ipsum deleniti porro ab impedit consequuntur dolores!
+                Amet quidem vel debitis a nobis?
+              </p>
             </Typography>
-        </Box>
+
+            <div className="mt-5">
+              <Typography variant="body1">
+                <div className="text-gray-400 space-y-1">
+                  <div className="flex">
+                    <p className="w-[25%] flex items-center gap-1">
+                      <FaHandPointRight className="text-blue-400" />
+                      Agency
+                    </p>
+                    <p className="w-[10%]">:</p>
+                    <p>All Bangladeshi Real Estate</p>
+                  </div>
+                  <div className="flex">
+                    <p className="w-[25%] flex items-center gap-1">
+                      <FaHandPointRight className="text-blue-400" />
+                      Agent Licence
+                    </p>
+                    <p className="w-[10%]">:</p>
+                    <p>1234 8562 6845 12547</p>
+                  </div>
+                  <div className="flex">
+                    <p className="w-[25%] flex items-center gap-1">
+                      <FaHandPointRight className="text-blue-400" />
+                      Tax Number
+                    </p>
+                    <p className="w-[10%]">:</p>
+                    <p>TN1257 RG54 85IU</p>
+                  </div>
+                  <div className="flex">
+                    <p className="w-[25%] flex items-center gap-1">
+                      <FaHandPointRight className="text-blue-400" />
+                      Service area
+                    </p>
+                    <p className="w-[10%]">:</p>
+                    <p>Bangladesh</p>
+                  </div>
+                </div>
+              </Typography>
+            </div>
+
+            <div className="my-5 pr-10">
+              <Divider />
+            </div>
+
+            <Typography variant="h5">
+              <p className="font-semibold">Property Status</p>
+            </Typography>
+
+            <div className="grid md:grid-cols-2 mt-5">
+              <div>
+                <PieChart
+                  series={[
+                    {
+                      data: [
+                        { id: 0, value: 10, label: "Total Listing" },
+                        { id: 1, value: 15, label: "Property Sold" },
+                        { id: 2, value: 20, label: "Property Rent" },
+                      ],
+                      highlightScope: { faded: "global", highlighted: "item" },
+                      faded: {
+                        innerRadius: 30,
+                        additionalRadius: -30,
+                        color: "gray",
+                      },
+                    },
+                  ]}
+                  height={150}
+                />
+              </div>
+              <div>
+                <PieChart
+                colors={palette}
+                  series={[
+                    {
+                      data: [
+                        { id: 0, value: 30, label: "Total Add" },
+                        { id: 1, value: 19, label: "Active Add" },
+                        { id: 2, value: 11, label: "Pending Add" },
+                      ],
+                      highlightScope: { faded: "global", highlighted: "item" },
+                      faded: {
+                        innerRadius: 30,
+                        additionalRadius: -30,
+                        color: "gray",
+                      },
+                    },
+                  ]}
+                  height={150}
+                />
+              </div>
+            </div>
+          </Box>
         </Box>
       </Box>
     </>
