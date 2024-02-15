23a47458c945899ebd80e9fa2441a9172a66cf91
MehediHasan128 <mehedihasan12926@gmail.com>
2024-02-16 02:03:57 +0600

    add property table page

--- src/Components/Pages/AgentDashBoard/MyProperties/MyProperties.jsx
+++ src/Components/Pages/AgentDashBoard/MyProperties/MyProperties.jsx
@@ -1,65 +1,16 @@
 import { Box, Typography } from "@mui/material";
-import { styled } from "@mui/material/styles";
-import Table from "@mui/material/Table";
-import TableBody from "@mui/material/TableBody";
-import TableCell, { tableCellClasses } from "@mui/material/TableCell";
-import TableContainer from "@mui/material/TableContainer";
-import TableHead from "@mui/material/TableHead";
-import TableRow from "@mui/material/TableRow";
-import Paper from "@mui/material/Paper";
-
-const StyledTableCell = styled(TableCell)(({ theme }) => ({
-  [`&.${tableCellClasses.head}`]: {
-    backgroundColor: theme.palette.common.black,
-    color: theme.palette.common.white,
-  },
-  [`&.${tableCellClasses.body}`]: {
-    fontSize: 14,
-  },
-}));
-
-const StyledTableRow = styled(TableRow)(({ theme }) => ({
-  "&:nth-of-type(odd)": {
-    backgroundColor: theme.palette.action.hover,
-  },
-  // hide last border
-  "&:last-child td, &:last-child th": {
-    border: 0,
-  },
-}));
-
-const rows = [
-  {
-    name: "Milk",
-    calories: 10,
-    fat: 15,
-    carbs: 62,
-    protein: 56,
-  },
-  {
-    name: "Milk",
-    calories: 10,
-    fat: 15,
-    carbs: 62,
-    protein: 56,
-  },
-  {
-    name: "Milk",
-    calories: 10,
-    fat: 15,
-    carbs: 62,
-    protein: 56,
-  },
-  {
-    name: "Milk",
-    calories: 10,
-    fat: 15,
-    carbs: 62,
-    protein: 56,
-  },
-];
+import useProperties from "../../../Hooks/useProperties";
+import PropertyTable from "../PropertyTable/PropertyTable";
 
 const MyProperties = () => {
+  const [properties] = useProperties();
+  const activeProperties = properties.filter(
+    (property) => property.status == "active"
+  );
+  const pendingProperties = properties.filter(
+    (property) => property.status == "pending"
+  );
+
   return (
     <>
       <Box>
@@ -69,96 +20,24 @@ const MyProperties = () => {
           </Typography>
         </Box>
 
-
         {/* Pending Listing */}
         <Box className="p-5 bg-white rounded-xl mt-10">
           <Typography variant="h6">
             <p className="mb-3 text-pink-500">Pending Listing</p>
           </Typography>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell align="center">Property Image</StyledTableCell>
-                  <StyledTableCell align="center">Property Title</StyledTableCell>
-                  <StyledTableCell align="center">Property Location</StyledTableCell>
-                  <StyledTableCell align="center">Property Type</StyledTableCell>
-                  <StyledTableCell align="center">Sell Or Rent</StyledTableCell>
-                  <StyledTableCell align="center">Actions</StyledTableCell>
-                  
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <StyledTableRow key={row.name}>
-                    <StyledTableCell align="center" component="th" scope="row">
-                      {row.name}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      {row.calories}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">{row.fat}</StyledTableCell>
-                    <StyledTableCell align="center">{row.carbs}</StyledTableCell>
-                    <StyledTableCell align="center">
-                      {row.protein}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      <button>Delete</button>
-                    </StyledTableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+         
+         <PropertyTable filterProperties={pendingProperties} />
         </Box>
 
 
         {/* Active Listing */}
         <Box className="p-5 bg-white rounded-xl mt-10">
           <Typography variant="h6">
-            <p className="mb-3 text-green-500">Active Listing</p>
+            <p className="mb-3 text-pink-500">Active Listing</p>
           </Typography>
-          <TableContainer component={Paper}>
-            <Table sx={{ minWidth: 700 }} aria-label="customized table">
-              <TableHead>
-                <TableRow>
-                  <StyledTableCell align="center">Property Image</StyledTableCell>
-                  <StyledTableCell align="center">Property Title</StyledTableCell>
-                  <StyledTableCell align="center">Property Location</StyledTableCell>
-                  <StyledTableCell align="center">Property Type</StyledTableCell>
-                  <StyledTableCell align="center">Sell Or Rent</StyledTableCell>
-                  <StyledTableCell align="center">Actions</StyledTableCell>
-                  
-                </TableRow>
-              </TableHead>
-              <TableBody>
-                {rows.map((row) => (
-                  <StyledTableRow key={row.name}>
-                    <StyledTableCell align="center" component="th" scope="row">
-                      {row.name}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      {row.calories}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">{row.fat}</StyledTableCell>
-                    <StyledTableCell align="center">{row.carbs}</StyledTableCell>
-                    <StyledTableCell align="center">
-                      {row.protein}
-                    </StyledTableCell>
-                    <StyledTableCell align="center">
-                      <button>Delete</button>
-                    </StyledTableCell>
-                  </StyledTableRow>
-                ))}
-              </TableBody>
-            </Table>
-          </TableContainer>
+         
+         <PropertyTable filterProperties={activeProperties} />
         </Box>
-
-       
-        
-
-
       </Box>
     </>
   );
